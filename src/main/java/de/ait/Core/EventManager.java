241ba7908447b8aca573311ad95fe1d9838a3fbc
Arina Malko <malko.arina212@gmail.com>
2025-01-29 23:44:18 +0100

    Добавление классов Event и EventManager (есть что доделать: логирование, обработка исключений, документирование, дописать метод). Также нужно дописать встроенные методы учёта билетов, добавления и удаления артистов (Artur Begichev)

--- src/main/java/de/ait/Core/EventManager.java
+++ src/main/java/de/ait/Core/EventManager.java
@@ -1,4 +1,63 @@
 package de.ait.Core;
 
+import de.ait.Model.Event;
+
+import java.util.HashMap;
+
 public class EventManager {
+    private HashMap<String, Event> events; // список событий (String - id, Event - событие)
+
+    public EventManager() {
+        events = new HashMap<>();
+    }
+
+    public HashMap<String, Event> getEvents() {
+        return new HashMap<>(events);
+    }
+
+    public void addEvent(Event event) {
+        if (event == null) {
+            // обработка исключения
+            // логирование
+            System.out.println("Error! Event is null");
+        } else if ((event.getId() == null || event.getId().isEmpty())
+                || (event.getName() == null || event.getName().isEmpty())
+                || (event.getEventType() == null || event.getEventType().isEmpty())
+                || (event.getDate() == null
+                || event.getLocation() == null || event.getLocation().isEmpty())
+                || (event.getTicketCount() <= 0))  {
+            // обработка исключения
+            // логирование
+            System.out.println("Error! Event is invalid");
+        } else if (events.containsKey(event.getId())){
+            // обработка исключения
+            // логирование
+            System.out.println("Error! Event is already exists");
+        } else {
+            events.put(event.getId(), event);
+        }
+    }
+
+    public void removeEvent(Event event) {
+        if (event == null) {
+            // обработка исключения
+            // логирование
+            System.out.println("Error! Event is null");
+        } else if ((event.getId() == null || event.getId().isEmpty())
+                || (event.getName() == null || event.getName().isEmpty())
+                || (event.getEventType() == null || event.getEventType().isEmpty())
+                || (event.getDate() == null
+                || event.getLocation() == null || event.getLocation().isEmpty())
+                || (event.getTicketCount() <= 0))  {
+            // обработка исключения
+            // логирование
+            System.out.println("Error! Event is invalid");
+        } else if (!events.containsKey(event.getId())){
+            // обработка исключения
+            // логирование
+            System.out.println("Error! Event is not in the list");
+        } else {
+            events.remove(event.getId());
+        }
+    }
 }
--- src/main/java/de/ait/Model/Event.java
+++ src/main/java/de/ait/Model/Event.java
@@ -1,4 +1,93 @@
 package de.ait.Model;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+
 public class Event {
+    private String id; // уникальный идентификатор
+    private String name; // название
+    private String eventType; // тип события
+    private LocalDate date; // дата
+    private String location; // место проведения
+    private int ticketCount; // количество билетов
+    HashSet<String> artistList; // список артистов (String - имя артиста)
+
+    public Event(String id,
+                 String name,
+                 String eventType,
+                 LocalDate date,
+                 String location,
+                 int ticketCount) {
+        this.id = id;
+        this.name = name;
+        this.eventType = eventType;
+        this.date = date;
+        this.location = location;
+        this.ticketCount = ticketCount;
+        this.artistList = new HashSet<>();
+    }
+
+    public Event(String id,
+                 String name,
+                 String eventType,
+                 LocalDate date,
+                 String location,
+                 int ticketCount,
+                 HashSet<String> artistList) {
+        this(id, name, eventType, date, location, ticketCount);
+        this.artistList = artistList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getTicketCount() {
+        return ticketCount;
+    }
+
+    public HashSet<String> getArtistList() {
+        return new HashSet<>(artistList);
+    }
+
+    // метод учета билетов
+    public void sellTicket(int count) {
+        // TODO Artur Begichev
+    }
+
+    // добавление нового артиста
+    public void addArtist(String artistName) {
+        // TODO Artur Begichev
+    }
+
+    // удаление артиста
+    public void removeArtist(String artistName) {
+        // TODO Artur Begichev
+    }
+
+    // метод расчета прибыли и затрат
+    double calculateProfit(double income, double expenses) {
+        // double income; // доход
+        // double expenses; // затраты
+
+        // основная логика расчета прибыли, затрат и вывода результата
+        // TODO
+        return income - expenses;
+    }
 }
