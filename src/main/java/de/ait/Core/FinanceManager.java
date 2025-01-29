a128659aa7b5431f1e68f9cf72bc9c4b5fe271bf
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-01-29 23:04:21 +0100

    Добавлены classes Finance Record and FinanceManager (частично написан).

--- src/main/java/de/ait/Core/FinanceManager.java
+++ src/main/java/de/ait/Core/FinanceManager.java
@@ -1,4 +1,20 @@
 package de.ait.Core;
 
+
+/*
+Этот класс будет управлять финансовыми записями и реализовывать основную логику.
+ */
+
+
+import de.ait.Model.FinanceRecord;
+
 public class FinanceManager {
+    private List<FinanceRecord> records; // Список финансовых записей
+    private int nextId = 1; // Счетчик для уникальных идентификаторов
+
+    // Конструктор
+    public FinanceManager() {
+        records = new ArrayList<>();
+        loadData(); // Загрузка данных при старте
+    }
 }
--- src/main/java/de/ait/Model/FinanceRecord.java
+++ src/main/java/de/ait/Model/FinanceRecord.java
@@ -1,4 +1,55 @@
 package de.ait.Model;
+/*
+Этот класс будет хранить информацию о финансовых записях (доходы/расходы).
+ */
+
+import java.time.LocalDate;
 
 public class FinanceRecord {
+    private int id; // Уникальный идентификатор
+    private String type; // Тип записи: доход/расход
+    private double amount; // Сумма
+    private String description; // Описание
+    private LocalDate date; // Дата
+
+    // Конструктор
+    public FinanceRecord(int id, String type, double amount, String description, LocalDate date) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "FinanceRecord{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
