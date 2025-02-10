497d2cdbd9a9319aa6b00e70cd815a677ed2073b
Arina Malko <malko.arina212@gmail.com>
2025-02-10 21:01:04 +0100

    Доделано логирование, обработка исключений (добавлены пользовательские исключения EventAlreadyInListException и EventIsNotInListException) и документирование в классах Event и EventManager. Дописан метод подсчёта прибыли calculateProfit в классе Event. Также методы добавления, удаления события, удаления события по id, получения события по id и вывод списка событий в классе EventManager. В класс Event добавлены поля, описывающие информацию о билетах (кол-во проданных билетов, цена одного билета). Добавлена работы с библиотекой lombok (Getter и AllArgsConstructor). Осталось дописать Unit-tests

---
+++ src/main/java/de/ait/Exceptions/EventAlreadyInListException.java
@@ -0,0 +1,7 @@
+package de.ait.Exceptions;
+
+public class EventAlreadyInListException extends Exception {
+    public EventAlreadyInListException(String message) {
+        super(message);
+    }
+}
--- src/main/java/de/ait/Model/Event.java
+++ src/main/java/de/ait/Model/Event.java
@@ -1,93 +1,183 @@
 package de.ait.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
+/**
+ * Класс Event управляет списком артистов и продажей билетов.
+ * Содержит такую информацию о событии как: название, тип события,
+ * дату, место проведения, общее и проданное количество билетов,
+ * цену за один билет на данное мероприятие и список артистов
+ */
+@Slf4j
+@AllArgsConstructor
+@Getter
 public class Event {
     private String id; // уникальный идентификатор
     private String name; // название
     private String eventType; // тип события
     private LocalDate date; // дата
     private String location; // место проведения
-    private int ticketCount; // количество билетов
+    private int totalTicketCount; // количество билетов
+    private int soldTicketCount; // количество проданных билетов
+    private double ticketPrice; // цена одного билета
     HashSet<String> artistList; // список артистов (String - имя артиста)
 
-    public Event(String id,
-                 String name,
+    /**
+     * Создается новый объект события, если ещё не продано ни одного билета
+     * Уникальный идентификатор задается с помощью UUID (Universally Unique Identifier)
+     * @param name              Название
+     * @param eventType         Тип события
+     * @param date              Дата
+     * @param location          Место проведения
+     * @param totalTicketCount  Количество билетов
+     * @param ticketPrice       Цена одного билета
+     */
+    public Event(String name,
                  String eventType,
                  LocalDate date,
                  String location,
-                 int ticketCount) {
-        this.id = id;
+                 int totalTicketCount,
+                 double ticketPrice) {
+        this.id = UUID.randomUUID().toString(); // Генерация уникального идентификатора
         this.name = name;
         this.eventType = eventType;
         this.date = date;
         this.location = location;
-        this.ticketCount = ticketCount;
+        this.totalTicketCount = totalTicketCount;
+        this.soldTicketCount = 0;
+        this.ticketPrice = ticketPrice;
         this.artistList = new HashSet<>();
     }
 
-    public Event(String id,
-                 String name,
+    /**
+     * Создается новый объект события, когда известно количество проданных билетов
+     * Уникальный идентификатор задается с помощью UUID (Universally Unique Identifier)
+     * @param name              Название
+     * @param eventType         Тип события
+     * @param date              Дата
+     * @param location          Место проведения
+     * @param totalTicketCount  Количество билетов
+     * @param soldTicketCount   Количество проданных билетов
+     * @param ticketPrice       Цена одного билета
+     */
+    public Event(String name,
                  String eventType,
                  LocalDate date,
                  String location,
-                 int ticketCount,
-                 HashSet<String> artistList) {
-        this(id, name, eventType, date, location, ticketCount);
-        this.artistList = artistList;
+                 int totalTicketCount,
+                 int soldTicketCount,
+                 double ticketPrice) {
+        this.id = UUID.randomUUID().toString(); // Генерация уникального идентификатора
+        this.name = name;
+        this.eventType = eventType;
+        this.date = date;
+        this.location = location;
+        this.totalTicketCount = totalTicketCount;
+        this.soldTicketCount = soldTicketCount;
+        this.ticketPrice = ticketPrice;
+        this.artistList = new HashSet<>();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEventType() {
-        return eventType;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public int getTicketCount() {
-        return ticketCount;
-    }
-
+    /**
+     * Возврат копии списка артистов
+     * @return Копия списка артистов
+     */
     public HashSet<String> getArtistList() {
         return new HashSet<>(artistList);
     }
 
-    // метод учета билетов
+    /**
+     * Метод продажи билетов.
+     * @param count Количество билетов, которое нужно продать.
+     */
     public void sellTicket(int count) {
-        // TODO Artur Begichev
+        if (count <= 0) {
+            throw new IllegalArgumentException("Количество билетов должно быть больше 0.");
+        }
+        if (soldTicketCount + count > totalTicketCount) {
+            throw new IllegalArgumentException("Недостаточно билетов. Доступно только " + (totalTicketCount - soldTicketCount) + ".");
+        }
+        soldTicketCount += count;
+        System.out.println(count + " билетов продано. Осталось билетов: " + (totalTicketCount - soldTicketCount));
     }
 
-    // добавление нового артиста
+    /**
+     * Метод отображения информации о билетах.
+     */
+    public void printTicketInfo() {
+        System.out.println("Мероприятие: " + name);
+        System.out.println("Всего билетов: " + totalTicketCount);
+        System.out.println("Продано билетов: " + soldTicketCount);
+        System.out.println("Осталось билетов: " + (totalTicketCount - soldTicketCount));
+    }
+
+    /**
+     * Метод добавления артиста в мероприятие.
+     * @param artistName Имя артиста.
+     */
     public void addArtist(String artistName) {
-        // TODO Artur Begichev
+        if (artistName == null || artistName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя артиста не может быть пустым.");
+        }
+        if (!artistList.add(artistName)) {
+            throw new IllegalArgumentException("Артист уже добавлен в мероприятие.");
+        }
+        System.out.println("Артист " + artistName + " добавлен в мероприятие: " + name);
     }
 
-    // удаление артиста
+    /**
+     * Метод удаления артиста из мероприятия.
+     * @param artistName Имя артиста.
+     */
     public void removeArtist(String artistName) {
-        // TODO Artur Begichev
+        if (!artistList.contains(artistName)) {
+            throw new NoSuchElementException("Артист не найден в мероприятии.");
+        }
+        artistList.remove(artistName);
+        System.out.println("Артист " + artistName + " удален из мероприятия: " + name);
     }
 
-    // метод расчета прибыли и затрат
-    double calculateProfit(double income, double expenses) {
-        // double income; // доход
-        // double expenses; // затраты
+    /**
+     * Метод расчёта прибыли на основе цены билета и затрат
+     * @param income     Доходы
+     * @param expenses   Расходы
+     * @return           Чистая прибыль (значение меньше нуля - убыток)
+     */
+    public double calculateProfit(double income, double expenses) {
+        double totalIncome = soldTicketCount * ticketPrice; // Общий доход от продажи билетов
+        double profit = totalIncome - expenses;
+        if (profit > 0) {
+            System.out.println("Событие " + name + " вышло на прибыль в размере: " + profit);
+            log.info("Событие {} вышло на прибыль в размере: {}", name, profit);
+        } else if (profit == 0) {
+            System.out.println("Событие " + name + " вышло в ноль.");
+            log.info("Событие {} вышло в ноль.", name);
+        } else {
+            System.out.println("Событие " + name + " вышло на убыток в размере: " + Math.abs(profit));
+            log.info("Событие {} вышло на убыток в размере: {}", name, Math.abs(profit));
+        }
+        return profit;
+    }
 
-        // основная логика расчета прибыли, затрат и вывода результата
-        // TODO
-        return income - expenses;
+    /**
+     * Метод отображения всей информации о событии.
+     */
+    public void printEventInfo() {
+        System.out.println("Идентификатор: " + id);
+        System.out.println("Название: " + name);
+        System.out.println("Тип события: " + eventType);
+        System.out.println("Дата: " + date);
+        System.out.println("Место проведения: " + location);
+        System.out.println("Количество билетов: " + totalTicketCount);
+        System.out.println("Продано билетов: " + soldTicketCount);
+        System.out.println("Осталось билетов: " + (totalTicketCount - soldTicketCount));
+        System.out.println("Список артистов: " + artistList);
     }
 }
